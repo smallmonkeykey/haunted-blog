6f257d2381b460ab60209e56a99d0688abae565e
smallmonkeykey <sarinitakuoo@gmail.com>
2025-02-14 16:00:28 +0900

    他人のブログの編集ページにアクセスできないように変更

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,7 +3,8 @@
 class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
-  before_action :set_blog, only: %i[show edit update destroy]
+  before_action :set_blog, only: %i[show destroy]
+  before_action :correct_user, only: %i[edit update]
 
   def index
     term = params[:term].presence || ''
@@ -50,5 +51,9 @@ class BlogsController < ApplicationController
 
   def blog_params
     params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+  end
+
+  def correct_user
+    @blog = current_user.blogs.find_by(id: params[:id])
   end
 end
