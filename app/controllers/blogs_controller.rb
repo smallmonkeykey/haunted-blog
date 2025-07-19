6890e5bc26b9aacd6832ebce270218b5c8bb59c7
smallmonkeykey <sarinitakuoo@gmail.com>
2025-07-19 14:31:09 +0900

    railsらしい書き方に変更

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,7 +10,7 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog = Blog.published.or(Blog.where(user_id: current_user)).find_by!(id: params[:id])
+    @blog = Blog.published.or(Blog.where(user: current_user)).find(params[:id])
   end
 
   def new
