b173a86a7c11b7e89c2a5f5b33ce255351f302ce
smallmonkeykey <sarinitakuoo@gmail.com>
2025-07-25 09:06:05 +0900

    変数名をlike_query_term に変更しsanitize_sql_likeの用途を明確にした

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -10,8 +10,8 @@ class Blog < ApplicationRecord
   scope :published, -> { where('secret = FALSE') }
 
   scope :search, lambda { |term|
-    escaped_term = sanitize_sql_like(term.to_s)
-    where('title LIKE :term OR content LIKE :term', term: "%#{escaped_term}%")
+    like_query_term = sanitize_sql_like(term.to_s)
+    where('title LIKE :term OR content LIKE :term', term: "%#{like_query_term}%")
   }
 
   scope :default_order, -> { order(id: :desc) }
